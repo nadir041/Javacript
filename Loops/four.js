4c005310979535ac03050fafadab7effeda44ccb
Nadir Shabbir <nadir62347a@.com>
2026-02-12 21:50:02 +0500

    Add IIFE example and cleanup scope files

---
+++ Loops/four.js
@@ -0,0 +1,40 @@
+// for each Loop
+
+
+const coding = ["js", "ruby", "python", "java", "cpp"]
+
+// coding.forEach( function (item){
+//     console.log(item);
+// }
+// )
+
+
+
+
+// coding.forEach ((item) => {
+//     console.log(item);
+// })
+
+
+
+
+const myCoding = [
+    {
+        language: "javascript",
+        ext: "js"
+    },
+    {
+        language: "python",
+        ext: "py"
+    },
+    {
+        language: "C++",
+        ext: "Cpp"
+    }
+]
+
+
+myCoding.forEach( (item) => {
+    console.log(item.language);
+    
+})
