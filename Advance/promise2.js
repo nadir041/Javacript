35016ac69c1b761b2e8a2f346f8b1fa05646007e
Nadir Shabbir <nadir62347a@.com>
2026-02-28 02:55:21 +0500

    promise2 updated

--- Advance/promise2.js
+++ Advance/promise2.js
@@ -1,4 +1,35 @@
+// const GITHUB_API = 'https://api.github.com/users/nadir041';
+// const user = fetch(GITHUB_API);
+// console.log(user);
+// user.then(function(data){
+//     console.log(data);
 
-const cart = ["shoes", "pants", "shirt"];
+// })
 
-createOrder(cart); 
+const cart = ["shoes", "pants", "kurta"];
+
+// createOrder(cart, function (orderId) {
+//   processToPayment(orderId, function (paymentInfo) {
+//     showOrderSummary(paymentInfo, function () {
+//       updateWallet();
+//     });
+//   });
+// });
+
+
+
+// createOrder(cart)
+// .then(function (orderId) {
+//   return proceesToPayment(orderId)
+// })
+// .then(function (paymentInfo) {
+//   return showOrderSummary(paymentInfo)
+// })
+// .then(function () {  updateWallet();
+// });
+
+
+createOrder(cart)
+.then(orderId => processToPayment (orderId) )
+.then(paymentInfo => showOrderSummary (paymentInfo) )
+.then(() => updateWallet() )
